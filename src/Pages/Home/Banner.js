02108fa59124b0a2a258bbef90c034f24e9eed3f
shahnmah <shahn.mah@gmail.com>
2022-05-15 22:42:44 +0600

    banner added

---
+++ src/Pages/Home/Banner.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import chairImg from '../../assets/images/chair.png'
+import bgImg from '../../assets/images/bg.png'
+
+const Banner = () => {
+    return (
+        <div class="hero min-h-screen px-12" style={{ backgroundImage:`url(${bgImg})` }}>
+            <div class="hero-content flex-col lg:flex-row-reverse">
+                <img src={chairImg} class="max-w-sm rounded-lg shadow-2xl" />
+                <div>
+                    <h1 class="text-5xl font-bold">Your New Smile Starts Here</h1>
+                    <p class="py-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the</p>
+                    <button class="btn bg-gradient-to-r from-secondary to-primary border-0 text-white font-bold">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Pages/Shared/Navbar.js
+++ src/Pages/Shared/Navbar.js
@@ -11,8 +11,7 @@ const Navbar = () => {
         <li><Link to='/login'>Login</Link></li>
     </>
     return (
-        <div>
-            <div class="navbar bg-base-100">
+            <div class="navbar bg-base-100 px-12">
                 <div class="navbar-start">
                     <div class="dropdown">
                         <label tabindex="0" class="btn btn-ghost lg:hidden">
@@ -30,7 +29,6 @@ const Navbar = () => {
                     </ul>
                 </div>
             </div>
-        </div>
     );
 };
 
